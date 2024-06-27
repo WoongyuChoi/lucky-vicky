35dc63d717a116546c0109342a87fde8897e12d7
Anne-Hyeyeon <khy2106@naver.com>
2024-06-27 16:50:09 +0900

    setting ESLint, prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,9 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': 'error',
   },
-}
+};
