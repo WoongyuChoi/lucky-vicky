4834cc60df3c58c9424fd40574a2a0e121cfe824
Hyeyeon(Anne) <khy2106@naver.com>
2024-06-27 20:45:35 +0900

    Feat: Add Vercel Analytics

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>원영적 사고 변환기</h1>
+      {/* 기존 App 내용 */}
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
